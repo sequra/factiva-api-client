25ef04060fcae29bf22296c2752d87284aa8ab83
Sergio Infantes <sergio.infantes@sequra.es>
2022-01-31 13:21:05 +0100

    [RT-170] Create auth

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+Dir["./lib/**/*.rb"].sort.each { |f| require f }
+Dir["./spec/support/**/*.rb"].sort.each { |f| require f }
+
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
---
+++ spec/support/vcr_setup.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+require "vcr"
+
+VCR.configure do |c|
+  c.cassette_library_dir = "spec/vcr"
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+  c.allow_http_connections_when_no_cassette = false
+  c.ignore_localhost = true
+  c.filter_sensitive_data("<SECRET_CLIENT_ID>") { Factiva.configuration.client_id }
+  c.filter_sensitive_data("<SECRET_PASSWORD>") { Factiva.configuration.password }
+  c.filter_sensitive_data("<SECRET_USERNAME>") { URI.encode_www_form_component(Factiva.configuration.username) }
+end
