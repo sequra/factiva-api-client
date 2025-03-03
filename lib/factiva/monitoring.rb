9c5d92702c85a2876cf0e9757c8cc322234493c1
julienmn <julien.maisonneuve@sequra.es>
2025-03-03 15:53:32 +0100

    fix: Add missing stub for bulk_create_associations

--- lib/factiva/monitoring.rb
+++ lib/factiva/monitoring.rb
@@ -103,6 +103,7 @@ module Factiva
     class MockedRequest
       attr_reader :stubbed_create_case,
       :stubbed_create_association,
+      :stubbed_bulk_create_associations,
       :stubbed_update_association,
       :stubbed_delete_association,
       :stubbed_add_association_to_case,
@@ -114,6 +115,7 @@ module Factiva
 
       def initialize(stubbed_create_case,
         stubbed_create_association,
+        stubbed_bulk_create_associations,
         stubbed_update_association,
         stubbed_delete_association,
         stubbed_add_association_to_case,
@@ -125,6 +127,7 @@ module Factiva
       )
         @stubbed_create_case = stubbed_create_case
         @stubbed_create_association = stubbed_create_association
+        @stubbed_create_association = stubbed_bulk_create_associations
         @stubbed_update_association = stubbed_update_association
         @stubbed_delete_association = stubbed_delete_association
         @stubbed_add_association_to_case = stubbed_add_association_to_case
